69755454463a0982c2afb7375281cfef8875e7cb
Caio-4lberto <caioalbertodasilva@gmail.com>
2022-08-11 13:27:14 -0300

    Aulas de Componentes e Dados

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent implements OnInit {
+
+  name: string = 'Caio';
+  age: number = 24;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

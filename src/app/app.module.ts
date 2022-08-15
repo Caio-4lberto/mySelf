1e8e6fe71301b194748eba8baa87e3a0b475a1c9
Caio-4lberto <caioalbertodasilva@gmail.com>
2022-08-14 23:28:07 -0300

    Aulas assistidas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ParentComponent } from './components/parent/parent.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { EventsComponent } from './components/events/events.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent
+    FirstComponentComponent,
+    ParentComponent,
+    DirectivesComponent,
+    IfRenderComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,

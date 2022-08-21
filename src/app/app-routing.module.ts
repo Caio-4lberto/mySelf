af7dd96af46185657ced62de3c34bc3bc6a66c6f
Caio-4lberto <caioalbertodasilva@gmail.com>
2022-08-21 11:59:20 -0300

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { EventsComponent } from './components/events/events.component';
+import { ExampleApplicationInterfacesComponent } from './components/example-application-interfaces/example-application-interfaces.component';
+import { ExampleApplicationServicesComponent } from './components/example-application-services/example-application-services.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { ParentComponent } from './components/parent/parent.component';
+import { PipeOperatorsComponent } from './components/pipe-operators/pipe-operators.component';
+import { TwoWayDataBidingComponent } from './components/two-way-data-biding/two-way-data-biding.component';
 
-const routes: Routes = [];
+//ROTAS CRIADAS PARA CADA COMPONENTE 🔽
+const routes: Routes = [
+  {path: '', component: FirstComponentComponent},
+  {path: 'parent', component: ParentComponent},
+  {path: 'directives', component: DirectivesComponent},
+  {path: 'if-render', component: IfRenderComponent},
+  {path: 'events', component: EventsComponent},
+  {path: 'list-render', component: ListRenderComponent},
+  {path: 'example-application-interfaces', component: ExampleApplicationInterfacesComponent},
+  {path: 'pipe-operators', component: PipeOperatorsComponent},
+  {path: 'two-way-data-biding', component: TwoWayDataBidingComponent},
+  {path: 'example-application-services', component: ExampleApplicationServicesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
